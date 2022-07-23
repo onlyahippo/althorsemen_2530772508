c60619d662297aa42a40ab3053b996b6e525e322
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-07-23 13:28:52 -0500

    v5.45

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local game = Game()
 local sfx = SFXManager()
 
 local firstLoaded = true
-local loadText = "Alt Horsemen v5.44 (COMPLETE)"
+local loadText = "Alt Horsemen v5.45 (COMPLETE)"
 local loadTextFailed = "Alt Horsemen load failed (STAGEAPI Disabled)"
 
 ------------------------------------------------------
