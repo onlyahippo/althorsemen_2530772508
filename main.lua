eb8e44883da42d8b76ee0ddec8462f002b0b3585
onlyahippo <tommyadow2@gmail.com>
2022-08-21 13:26:25 -0400

    Version bump v5.6

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local game = Game()
 local sfx = SFXManager()
 
 local firstLoaded = true
-local loadText = "Alt Horsemen v5.5 (COMPLETE)"
+local loadText = "Alt Horsemen v5.6 (COMPLETE)"
 local loadTextFailed = "Alt Horsemen load failed (STAGEAPI Disabled)"
 
 ------------------------------------------------------
@@ -2671,7 +2671,7 @@ function mod:Death2AI(npc)
 		--transform
 		elseif d.substate == 1 then
 			if sprite:IsEventTriggered("Effect") then
-				local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, npc.Position, Vector.Zero, player):ToEffect()
+				local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, npc.Position, Vector.Zero, npc):ToEffect()
 				poof.Color = d.poofColor
 				npc.EntityCollisionClass = EntityCollisionClass.ENTCOLL_PLAYERONLY
 				
